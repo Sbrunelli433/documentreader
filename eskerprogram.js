558102c5b1c54bc02c701221f352b275180f476f
Steve Brunelli <brunes46@gmail.com>
2020-04-22 18:54:46 -0500

    initial commit with dev environment setup files.

---
+++ eskerprogram.js
@@ -0,0 +1,16 @@
+//write a function that takes in a .txt file 
+
+//function returns an output
+
+//console.log the following
+//File name:
+// Number of lines:
+// Number of characters (total):
+// Number of letters:
+// Number of figures:
+// Number of other characters:
+// Number of words (strings):
+// Number of 1 letter words (count of strings where int in string <2)
+// Number of 2 letter words (count of string where int in string >= 2)
+// Number of 16 letter words 
+// Number of 19 letter words
